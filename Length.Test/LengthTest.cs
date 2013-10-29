dde53f0f778bfa5ce2bb796529bffb3fa3f6ab1b
jksun <jksun@thoughtworks.com>
2013-10-29 19:13:06 +0800

    add covert function to the length

---
+++ Length.Test/LengthTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Length.Test
+{
+
+    public class LengthTest
+    {
+        [Fact]
+        public void should_get_a_legal_length()
+        {
+            var length = new Length(10.0, Unit.CentiMetre);
+            Assert.Equal(10.0, length.Value);
+            Assert.Equal(Unit.CentiMetre, length.Unit);
+        }
+
+        [Fact]
+        public void should_equal_when_value_unit_same()
+        {
+            var length1 = new Length(100.0, Unit.CentiMetre);
+            var length2 = new Length(100.0, Unit.CentiMetre);
+            Assert.True(length1.Equals(length2));
+
+            length1 = new Length(1.0, Unit.Metre);
+            length2 = new Length(1.0, Unit.Metre);
+            Assert.True(length1.Equals(length2));
+
+            length1 = new Length(10.0, Unit.MilliMetre);
+            length2 = new Length(10.0, Unit.MilliMetre);
+            Assert.True(length1.Equals(length2));
+        }
+
+        [Fact]
+        public void should_transmit_between_different_unit()
+        {
+            var tenMitre = new Length(10, Unit.Metre);
+            var aThousandCentiMetre = new Length(1000, Unit.CentiMetre);
+            Assert.Equal(aThousandCentiMetre, tenMitre.Convert(aThousandCentiMetre.Unit));
+        }
+    }
+}
---
+++ Length/Length.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Length
+{
+    public class Length
+    {
+        
+
+        public Length(double value, Unit unit)
+        {
+            Unit = unit;
+            Value = value;
+        }
+
+        public double Value { get; private set; }
+
+        public Unit Unit { get; private set; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Length) obj);
+        }
+
+        protected bool Equals(Length length)
+        {
+            return Value.Equals(length.Value) && Unit == length.Unit;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Value.GetHashCode() * 397) ^ (int)Unit;
+            }
+        }
+
+        public Length Convert(Unit unit)
+        {
+            if (this.Unit == unit)
+            {
+                return this;
+            }
+            else
+            {
+                if (unit == Unit.CentiMetre && this.Unit == Unit.Metre)
+                {
+                    var transmit = new Length(this.Value*100, unit);
+                    return transmit;
+                }
+                return null;
+            }
+        }
+    }
+
+    public enum Unit
+    {
+        MilliMetre,
+        CentiMetre,
+        Metre
+    }
+}
